a1a2767393d5c8b89cd6399735052c2a1b7dc4b8
vyshnavisree21 <vyshnavisree21@gmail.com>
2022-06-19 19:29:56 +0530

    sabd

--- backend/src/config.js
+++ backend/src/config.js
@@ -7,24 +7,102 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "YOUR COLLECTION NAME";
-const description = "Remember to replace this description";
+const namePrefix = "AHS";
+const description = "ALIEN HIGH SOCIETY";
 const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
+    growEditionSizeTo: 1,
+    layersOrder: [
+      
+      { name: "Head 1" },
+      {name:"Hoddies"},
+      {name:"Logo"},
+          
+      ],
+  }, 
+  {
+    growEditionSizeTo: 2,
+    layersOrder: [
+      
+      { name: "head2" },
+      {name:"Shirts 2"},
+          
+      ],
+  },   
+  {
+    growEditionSizeTo: 3,
+    layersOrder: [
+      
+      { name: "head2" },
+      {name:"Shirts 2"},
+      {name:"Glass Mask"},
+          
+      ],
+  },   
+  {
+    growEditionSizeTo: 4,
+    layersOrder: [
+      
+      { name: "cyclops" },
+  
+      {name:"Glass Mask"},
+      {name:"Hoddie"},
+      {name:"Logos"},
+          
+      ],
+  },   
+  {
     growEditionSizeTo: 5,
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
-    ],
-  },
+      
+      { name: "cyclops" },    
+      {name:"Hoddie"},
+      {name:"Logos"},
+          
+      ],
+  },   
+  {
+    growEditionSizeTo: 6,
+    layersOrder: [
+      
+      { name: "Head With Jacket 4" },
+      {name:"Skull"},
+          
+      ],
+  },   
+  {
+    growEditionSizeTo: 7,
+    layersOrder: [
+      {name:"gun 5"},
+      { name: "head 5" },
+      {name:"skull"},
+     
+          
+      ],
+  },   
+  {
+    growEditionSizeTo: 8,
+    layersOrder: [
+      
+      { name: "head6" },
+      {name:"glasses"},
+      {name:"Closed mask"},
+          
+      ],
+  }, 
+      
+  {
+    growEditionSizeTo: 9,
+    layersOrder: [
+      
+      { name: "head 7" },
+      {name:"glass masks"},
+          
+      ],
+  },     
 ];
 
 const shuffleLayerConfigurations = true;
@@ -32,13 +110,13 @@ const shuffleLayerConfigurations = true;
 const debugLogs = false;
 
 const format = {
-  width: 512,
-  height: 512,
+  width: 2048,
+  height: 2048,
   smoothing: false,
 };
 
 const extraMetadata = {
-  external_url: "https://codecats.xyz", // Replace with your website or remove this line if you do not have one.
+  external_url: "https://www.alienhighsociety.com", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
@@ -49,22 +127,22 @@ const LIMIT = 2; // Your API key rate limit
 const CHAIN = 'rinkeby'; // only rinkeby or polygon
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = 'CRYPTOPUNKS';
-const CONTRACT_SYMBOL = 'CP';
-const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
-const OWNER_ADDRESS = 'YOUR WALLET ADDRESS HERE';
-const TREASURY_ADDRESS = 'YOUR WALLET ADDRESS HERE';
-const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
+const CONTRACT_NAME = 'ALIEN HIGH SOCIETY';
+const CONTRACT_SYMBOL = 'AHS';
+const METADATA_UPDATABLE = false; // set to false if you don't want to allow metadata updates after minting
+const OWNER_ADDRESS = '0x041286cf38935D181352bdd9B17f6DA6B0ca9a3D';
+const TREASURY_ADDRESS = '0x041286cf38935D181352bdd9B17f6DA6B0ca9a3D';
+const MAX_SUPPLY = 9; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
 const MINT_PRICE = 1; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
-const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
+const TOKENS_PER_MINT = 2; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-06-21T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
-const ROYALTY_ADDRESS = "0xd8B808A887326F45B2D0cd999709Aa6264CeF919"; // Address that will receive the royalty
+const ROYALTY_ADDRESS = "0x041286cf38935D181352bdd9B17f6DA6B0ca9a3D"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
@@ -76,7 +154,7 @@ let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually inclu
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
 const GENERIC_DESCRIPTION = "REPLACE THIS"; // Replace with what you want the generic descriptions to say.
-const GENERIC_IMAGE = "https://ipfs.io/ipfs/QmUf9tDbkqnfHkQaMdFWSGAeXwVXWA61pFED7ypx4hcsfh"; // Replace with your generic image that will display for all NFTs pre-reveal.
+const GENERIC_IMAGE = "https://ipfs.io/ipfs/bafkreibskhnn7d53765li7vkvnd54gpeajnmq372dytv7a5ukrxtqjk5s4"; // Replace with your generic image that will display for all NFTs pre-reveal.
 
 // Automatically set contract address if deployed using the deployContract.js script
 try {
@@ -93,12 +171,12 @@ try {
 // END NFTPort Info
 
 const solanaMetadata = {
-  symbol: "YC",
+  symbol: "AHS",
   seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
   external_url: "https://www.youtube.com/c/hashlipsnft",
   creators: [
     {
-      address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
+      address: "0x041286cf38935D181352bdd9B17f6DA6B0ca9a3D",
       share: 100,
     },
   ],
