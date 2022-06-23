43c363dffbcdb2a5121e5191f4d86fe4ad322f49
nithien7 <nithien7@gmail.com>
2022-06-23 08:05:06 +0530

    a

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -228,7 +228,7 @@ async function loadInfo() {
   }
   const price = web3.utils.fromWei(info.deploymentConfig.mintPrice, 'ether');
   const pricePerMint = document.getElementById("pricePerMint");
-  const maxPerMint = document.getElementById("maxPerMint");
+  const maxPerMint = document.getElementsByClassName("Wall");
   const totalSupply = document.getElementById("totalSupply");
   const mintInput = document.getElementById("mintInput");
   
